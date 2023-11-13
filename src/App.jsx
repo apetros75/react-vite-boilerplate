957a8b660d7eea653988f570964f363af4e29464
Arthur Petrosyan <apetrosviiv@gmail.com>
2023-11-14 01:56:52 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import './App.css'
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <p className="">Template for Pre-configured Vite Project</p>
+      </div>
+    </>
+  );
+};
